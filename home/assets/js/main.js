bfe5b7a01a06e29cdf28a19bdd9389173adbc5aa
PetersonDias01 <peterson.odias@gmail.com>
2024-11-14 00:21:01 -0300

    Update main.js

--- home/assets/js/main.js
+++ home/assets/js/main.js
@@ -9,22 +9,22 @@ new Vue({
     comentarios: [
       {
         img: "./assets/img/1.webp",
-        nome: "Lia",
+        nome: "Jennifer",
         texto: "I have been struggling with diabetes for years. I really need help. But seeing this video gives me hope that I might finally find the solution I've been looking for.",
       },
       {
         img: "./assets/img/2.webp",
-        nome: "Herbert",
+        nome: "Michael",
         texto: "I know how tough it can be. I was in the same situation, feeling hopeless, but this approach truly helped me stabilize my blood sugar levels. I wish I had found it sooner!",
       },
       {
         img: "./assets/img/3.webp",
-        nome: "Raul",
+        nome: "Richard S.",
         texto: "I felt hopeless with my diabetes, always tired and frustrated, until I found this method. It really worked for me. I think this video might help many others too. Stay positive!",
       },
       {
         img: "./assets/img/4.webp",
-        nome: "Maria R.",
+        nome: "Barbara R.",
         texto: "If you're struggling, just know that you're not alone. I was in the same place, feeling like there was no way out, but finally found a natural way to control it. This could be the turning point for you too!",
       },
       {
@@ -34,22 +34,22 @@ new Vue({
       },
       {
         img: "./assets/img/6.webp",
-        nome: "Ester R",
+        nome: "Sarah R",
         texto: "I can't believe how much better I feel now. I wish I had known about this sooner. Watching this makes me hopeful that others can find the same kind of help.",
       },
       {
         img: "./assets/img/7.webp",
-        nome: "Love2",
+        nome: "Mark Hernandez",
         texto: "My friend recommended something that completely changed my diabetes management. I see similar things being shared here. Never lose hope, this could be your answer!",
       },
       {
         img: "./assets/img/8.webp",
-        nome: "Brunno",
+        nome: "Anthony",
         texto: "For anyone still struggling: I used to feel helpless too. I thought I'd never get better, but then I found a solution that made a difference. Don't give up! This video might be what you need.",
       },
       {
         img: "./assets/img/9.webp",
-        nome: "Leo",
+        nome: "David Miller",
         texto: "It took time and felt impossible, but I finally managed to get my blood sugar under control. There is a way, and I hope this video can help you find it too!",
       },
       {
@@ -64,7 +64,7 @@ new Vue({
       },
       {
         img: "./assets/img/12.webp",
-        nome: "The Weed",
+        nome: "Steve Lee",
         texto: "Trust me, there are ways to manage this. I found something that worked for me, and it looks like this video might help you find something too. Keep the faith!",
       },
       {
@@ -74,17 +74,17 @@ new Vue({
       },
       {
         img: "./assets/img/14.webp",
-        nome: "Roque",
+        nome: "Christopher",
         texto: "I thought I had tried everything. But there's always something new to learn. Finding the right approach made all the difference for me. This video could be the one thing you haven't tried yet.",
       },
       {
         img: "./assets/img/15.webp",
-        nome: "Thiago",
+        nome: "Paul Jackson",
         texto: "Don't be discouraged. I felt lost too, until I found this new approach. Now, I feel so much better. Maybe this video is the help you need.",
       },
       {
         img: "./assets/img/16.webp",
-        nome: "Rodrygo ",
+        nome: "Daniel Anderson ",
         texto: "To anyone feeling overwhelmed: I was in your shoes, but I found something that worked. I believe this video could help you too. Keep pushing through!",
       },
       {
@@ -94,17 +94,17 @@ new Vue({
       },
       {
         img: "./assets/img/18.webp",
-        nome: "Alone",
+        nome: "Linda Brown",
         texto: "I know the pain of feeling stuck with diabetes. Finding the right help was a game-changer for me. Maybe this video can be that help for you.",
       },
       {
         img: "./assets/img/19.webp",
-        nome: "Mixely",
+        nome: "Betty Taylor",
         texto: "There are people out here who understand. I found support, and you can too. This video could be the first step to getting the support you need.",
       },
       {
         img: "./assets/img/20.webp",
-        nome: "Pale Man",
+        nome: "Andrew Perez",
         texto: "Diabetes doesn't have to control you forever. There's always hope for change. Maybe this video will show you the way.",
       },
     ],
